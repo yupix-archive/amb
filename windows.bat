9e6b7aa0791ca4a0c3066634bd3cac1686d9626a
yupix <50538210+yupix@users.noreply.github.com>
2020-02-18 20:47:33 +0900

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell if(where wsl){wsl ./linux.sh}Else{Write-Host "not found 'wsl'"}
+powershell if(where wsl){Write-Host "found 'wsl'" && Write-Host "お使いのwslで動作可能です。"}Else{Write-Host "not found 'wsl'"}
